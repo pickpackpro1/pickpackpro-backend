1102a583a50d3b3d5e6c9b84ef8ed289cb24d595
NoorKhan2233 <203442216+NoorKhan2233@users.noreply.github.com>
2026-05-15 15:15:23 +0500

    fixing CORS policy

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization, X-User-Id, Cache-Control",
+            value: "Content-Type, Authorization, X-User-Id, Cache-Control, Pragma",
           },
           {
             key: "Access-Control-Allow-Credentials",
